452d3582b402d60f90e3d50d232df3a1027ee46c
shafayet <shafayethossain777@gmail.com>
2022-07-21 00:38:52 +0600

    css changed

--- src/components/Position.jsx
+++ src/components/Position.jsx
@@ -12,179 +12,179 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 export default function Position() {
-    const [accordion,setAccordion]=useState('')
+  const [accordion, setAccordion] = useState('')
 
-    const chanegHandler=(value)=>{
-        console.log(value)
-        if(accordion==value){
-            setAccordion('')
-            return null
-        }
-        setAccordion(value)
+  const chanegHandler = (value) => {
+    console.log(value)
+    if (accordion == value) {
+      setAccordion('')
+      return null
     }
+    setAccordion(value)
+  }
   return <Box mt={4}>
-      <hr style={{width:'60px',borderWidth:'4px',border:"2px solid #5BBC2E"}}/>
-      <Typography sx={{fontSize:'34px',textAlign:'center'}}>
+    <hr style={{ width: '60px', borderWidth: '4px', border: "2px solid #5BBC2E" }} />
+    <Typography sx={{ fontSize: '34px', textAlign: 'center' }}>
       BROWSE OPEN POSITIONS BY CATEGORY
 
-      </Typography>
+    </Typography>
 
-      <Typography sx={{textAlign:'center'}}>
+    <Typography sx={{ textAlign: 'center' }}>
       We are always on the lookout for talanted people
-      </Typography>
-      <Container maxWidth="md">
-      <Accordion  onChange={()=>chanegHandler('panel1')}  expanded={accordion=='panel1'}>
+    </Typography>
+    <Container maxWidth="md">
+      <Accordion onChange={() => chanegHandler('panel1')} expanded={accordion == 'panel1'}>
         <AccordionSummary
-          expandIcon={ accordion=="panel1"?<RemoveIcon/>:<AddIcon/>}
+          expandIcon={accordion == "panel1" ? <RemoveIcon /> : <AddIcon />}
           aria-controls="panel1a-content"
           id="panel-1"
-          onChange={()=>chanegHandler('panel1')}
+          onChange={() => chanegHandler('panel1')}
         >
           <Typography>Sales & Marketting</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <List>
-              <ListItem sx={{backgroundColor:'#FFFFFF',marginBottom:"5px",display:'flex',justifyContent:'space-between'}}>
-                  <Typography>Sales Manager</Typography>
-                  <Button variant="outlined" sx={{borderRadius:'1px solid rgba(0, 0, 0, 0.1)',backgroundColor:'rgb(255, 255, 255)',border:'1px solid #80808080',color:'rgba(0, 0, 0, 0.6)',"&:hover":{backgroundColor:'#182F59',color:'white'}}}>Apply Now</Button>
+            <ListItem sx={{ backgroundColor: '#FFFFFF', marginBottom: "5px", display: 'flex', justifyContent: 'space-between' }}>
+              <Typography>Sales Manager</Typography>
+              <Button variant="outlined" sx={{ borderRadius: '1px solid rgba(0, 0, 0, 0.1)', backgroundColor: 'rgb(255, 255, 255)', border: '1px solid #80808080', color: 'rgba(0, 0, 0, 0.6)', "&:hover": { backgroundColor: '#182F59', color: 'white' } }}>Apply Now</Button>
 
-              </ListItem>
-              <ListItem sx={{backgroundColor:'#FFFFFF',marginBottom:"5px",display:'flex',justifyContent:'space-between'}}>
-                  <Typography>Sales Executive</Typography>
-                  <Button variant="outlined" sx={{borderRadius:'1px solid rgba(0, 0, 0, 0.1)',backgroundColor:'rgb(255, 255, 255)',border:'1px solid #80808080',color:'rgba(0, 0, 0, 0.6)',"&:hover":{backgroundColor:'#182F59',color:'white'}}}>Apply Now</Button>
+            </ListItem>
+            <ListItem sx={{ backgroundColor: '#FFFFFF', marginBottom: "5px", display: 'flex', justifyContent: 'space-between' }}>
+              <Typography>Sales Executive</Typography>
+              <Button variant="outlined" sx={{ borderRadius: '1px solid rgba(0, 0, 0, 0.1)', backgroundColor: 'rgb(255, 255, 255)', border: '1px solid #80808080', color: 'rgba(0, 0, 0, 0.6)', "&:hover": { backgroundColor: '#182F59', color: 'white' } }}>Apply Now</Button>
 
-              </ListItem>
+            </ListItem>
           </List>
         </AccordionDetails>
       </Accordion>
-      <Accordion    onChange={()=>chanegHandler('panel2')} expanded={accordion=='panel2'}>
+      <Accordion onChange={() => chanegHandler('panel2')} expanded={accordion == 'panel2'}>
         <AccordionSummary
-         expandIcon={ accordion=="panel2"?<RemoveIcon/>:<AddIcon/>} 
+          expandIcon={accordion == "panel2" ? <RemoveIcon /> : <AddIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
-        
+
         >
           <Typography>Human Resource</Typography>
         </AccordionSummary>
         <AccordionDetails>
-        <List>
-              <ListItem sx={{backgroundColor:'#FFFFFF',marginBottom:"5px",display:'flex',justifyContent:'space-between'}}>
-                  <Typography>Sales Executive</Typography>
-                  <Button variant="outlined" sx={{borderRadius:'1px solid rgba(0, 0, 0, 0.1)',backgroundColor:'rgb(255, 255, 255)',border:'1px solid #80808080',color:'rgba(0, 0, 0, 0.6)',"&:hover":{backgroundColor:'#182F59',color:'white'}}}>Apply Now</Button>
+          <List>
+            <ListItem sx={{ backgroundColor: '#FFFFFF', marginBottom: "5px", display: 'flex', justifyContent: 'space-between' }}>
+              <Typography>Sales Executive</Typography>
+              <Button variant="outlined" sx={{ borderRadius: '1px solid rgba(0, 0, 0, 0.1)', backgroundColor: 'rgb(255, 255, 255)', border: '1px solid #80808080', color: 'rgba(0, 0, 0, 0.6)', "&:hover": { backgroundColor: '#182F59', color: 'white' } }}>Apply Now</Button>
 
-              </ListItem>
-              <ListItem sx={{backgroundColor:'#FFFFFF',marginBottom:"5px",display:'flex',justifyContent:'space-between'}}>
-                  <Typography>Sales Executive</Typography>
-                  <Button variant="outlined" sx={{borderRadius:'1px solid rgba(0, 0, 0, 0.1)',backgroundColor:'rgb(255, 255, 255)',border:'1px solid #80808080',color:'rgba(0, 0, 0, 0.6)',"&:hover":{backgroundColor:'#182F59',color:'white'}}}>Apply Now</Button>
+            </ListItem>
+            <ListItem sx={{ backgroundColor: '#FFFFFF', marginBottom: "5px", display: 'flex', justifyContent: 'space-between' }}>
+              <Typography>Sales Executive</Typography>
+              <Button variant="outlined" sx={{ borderRadius: '1px solid rgba(0, 0, 0, 0.1)', backgroundColor: 'rgb(255, 255, 255)', border: '1px solid #80808080', color: 'rgba(0, 0, 0, 0.6)', "&:hover": { backgroundColor: '#182F59', color: 'white' } }}>Apply Now</Button>
 
-              </ListItem>
+            </ListItem>
           </List>
         </AccordionDetails>
       </Accordion>
-      <Accordion    onChange={()=>chanegHandler('panel3')} expanded={accordion=='panel3'}>
+      <Accordion onChange={() => chanegHandler('panel3')} expanded={accordion == 'panel3'}>
         <AccordionSummary
-         expandIcon={ accordion=="panel3"?<RemoveIcon/>:<AddIcon/>} 
+          expandIcon={accordion == "panel3" ? <RemoveIcon /> : <AddIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
-        
+
         >
           <Typography>Administration</Typography>
         </AccordionSummary>
         <AccordionDetails>
-        <List>
-              <ListItem sx={{backgroundColor:'#FFFFFF',marginBottom:"5px",display:'flex',justifyContent:'space-between'}}>
-                  <Typography></Typography>
-                  <Button variant="outlined" sx={{borderRadius:'1px solid rgba(0, 0, 0, 0.1)',backgroundColor:'rgb(255, 255, 255)',border:'1px solid #80808080',color:'rgba(0, 0, 0, 0.6)',"&:hover":{backgroundColor:'#182F59',color:'white'}}}>Apply Now</Button>
+          <List>
+            <ListItem sx={{ backgroundColor: '#FFFFFF', marginBottom: "5px", display: 'flex', justifyContent: 'space-between' }}>
+              <Typography></Typography>
+              <Button variant="outlined" sx={{ borderRadius: '1px solid rgba(0, 0, 0, 0.1)', backgroundColor: 'rgb(255, 255, 255)', border: '1px solid #80808080', color: 'rgba(0, 0, 0, 0.6)', "&:hover": { backgroundColor: '#182F59', color: 'white' } }}>Apply Now</Button>
 
-              </ListItem>
-              <ListItem sx={{backgroundColor:'#FFFFFF',marginBottom:"5px",display:'flex',justifyContent:'space-between'}}>
-                  <Typography>Sales Executive</Typography>
-                  <Button variant="outlined" sx={{borderRadius:'1px solid rgba(0, 0, 0, 0.1)',backgroundColor:'rgb(255, 255, 255)',border:'1px solid #80808080',color:'rgba(0, 0, 0, 0.6)',"&:hover":{backgroundColor:'#182F59',color:'white'}}}>Apply Now</Button>
+            </ListItem>
+            <ListItem sx={{ backgroundColor: '#FFFFFF', marginBottom: "5px", display: 'flex', justifyContent: 'space-between' }}>
+              <Typography>Sales Executive</Typography>
+              <Button variant="outlined" sx={{ borderRadius: '1px solid rgba(0, 0, 0, 0.1)', backgroundColor: 'rgb(255, 255, 255)', border: '1px solid #80808080', color: 'rgba(0, 0, 0, 0.6)', "&:hover": { backgroundColor: '#182F59', color: 'white' } }}>Apply Now</Button>
 
-              </ListItem>
+            </ListItem>
           </List>
         </AccordionDetails>
       </Accordion>
 
 
 
-      <Accordion    onChange={()=>chanegHandler('panel4')} expanded={accordion=='panel4'}>
+      <Accordion onChange={() => chanegHandler('panel4')} expanded={accordion == 'panel4'}>
         <AccordionSummary
-         expandIcon={ accordion=="panel4"?<RemoveIcon/>:<AddIcon/>} 
+          expandIcon={accordion == "panel4" ? <RemoveIcon /> : <AddIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
-        
+
         >
           <Typography>Creative</Typography>
         </AccordionSummary>
         <AccordionDetails>
-        <List>
-              <ListItem sx={{backgroundColor:'#FFFFFF',marginBottom:"5px",display:'flex',justifyContent:'space-between'}}>
-                  <Typography></Typography>
-                  <Button variant="outlined" sx={{borderRadius:'1px solid rgba(0, 0, 0, 0.1)',backgroundColor:'rgb(255, 255, 255)',border:'1px solid #80808080',color:'rgba(0, 0, 0, 0.6)',"&:hover":{backgroundColor:'#182F59',color:'white'}}}>Apply Now</Button>
+          <List>
+            <ListItem sx={{ backgroundColor: '#FFFFFF', marginBottom: "5px", display: 'flex', justifyContent: 'space-between' }}>
+              <Typography></Typography>
+              <Button variant="outlined" sx={{ borderRadius: '1px solid rgba(0, 0, 0, 0.1)', backgroundColor: 'rgb(255, 255, 255)', border: '1px solid #80808080', color: 'rgba(0, 0, 0, 0.6)', "&:hover": { backgroundColor: '#182F59', color: 'white' } }}>Apply Now</Button>
 
-              </ListItem>
-              <ListItem sx={{backgroundColor:'#FFFFFF',marginBottom:"5px",display:'flex',justifyContent:'space-between'}}>
-                  <Typography>Sales Executive</Typography>
-                  <Button variant="outlined" sx={{borderRadius:'1px solid rgba(0, 0, 0, 0.1)',backgroundColor:'rgb(255, 255, 255)',border:'1px solid #80808080',color:'rgba(0, 0, 0, 0.6)',"&:hover":{backgroundColor:'#182F59',color:'white'}}}>Apply Now</Button>
+            </ListItem>
+            <ListItem sx={{ backgroundColor: '#FFFFFF', marginBottom: "5px", display: 'flex', justifyContent: 'space-between' }}>
+              <Typography>Sales Executive</Typography>
+              <Button variant="outlined" sx={{ borderRadius: '1px solid rgba(0, 0, 0, 0.1)', backgroundColor: 'rgb(255, 255, 255)', border: '1px solid #80808080', color: 'rgba(0, 0, 0, 0.6)', "&:hover": { backgroundColor: '#182F59', color: 'white' } }}>Apply Now</Button>
 
-              </ListItem>
+            </ListItem>
           </List>
         </AccordionDetails>
       </Accordion>
 
-      
-      <Accordion    onChange={()=>chanegHandler('panel5')} expanded={accordion=='panel5'}>
+
+      <Accordion onChange={() => chanegHandler('panel5')} expanded={accordion == 'panel5'}>
         <AccordionSummary
-         expandIcon={ accordion=="panel5"?<RemoveIcon/>:<AddIcon/>} 
+          expandIcon={accordion == "panel5" ? <RemoveIcon /> : <AddIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
-        
+
         >
           <Typography>Digital Marketing</Typography>
         </AccordionSummary>
         <AccordionDetails>
-        <List>
-              <ListItem sx={{backgroundColor:'#FFFFFF',marginBottom:"5px",display:'flex',justifyContent:'space-between'}}>
-                  <Typography></Typography>
-                  <Button variant="outlined" sx={{borderRadius:'1px solid rgba(0, 0, 0, 0.1)',backgroundColor:'rgb(255, 255, 255)',border:'1px solid #80808080',color:'rgba(0, 0, 0, 0.6)',"&:hover":{backgroundColor:'#182F59',color:'white'}}}>Apply Now</Button>
+          <List>
+            <ListItem sx={{ backgroundColor: '#FFFFFF', marginBottom: "5px", display: 'flex', justifyContent: 'space-between' }}>
+              <Typography></Typography>
+              <Button variant="outlined" sx={{ borderRadius: '1px solid rgba(0, 0, 0, 0.1)', backgroundColor: 'rgb(255, 255, 255)', border: '1px solid #80808080', color: 'rgba(0, 0, 0, 0.6)', "&:hover": { backgroundColor: '#182F59', color: 'white' } }}>Apply Now</Button>
 
-              </ListItem>
-              <ListItem sx={{backgroundColor:'#FFFFFF',marginBottom:"5px",display:'flex',justifyContent:'space-between'}}>
-                  <Typography>Sales Executive</Typography>
-                  <Button variant="outlined" sx={{borderRadius:'1px solid rgba(0, 0, 0, 0.1)',backgroundColor:'rgb(255, 255, 255)',border:'1px solid #80808080',color:'rgba(0, 0, 0, 0.6)',"&:hover":{backgroundColor:'#182F59',color:'white'}}}>Apply Now</Button>
+            </ListItem>
+            <ListItem sx={{ backgroundColor: '#FFFFFF', marginBottom: "5px", display: 'flex', justifyContent: 'space-between' }}>
+              <Typography>Sales Executive</Typography>
+              <Button variant="outlined" sx={{ borderRadius: '1px solid rgba(0, 0, 0, 0.1)', backgroundColor: 'rgb(255, 255, 255)', border: '1px solid #80808080', color: 'rgba(0, 0, 0, 0.6)', "&:hover": { backgroundColor: '#182F59', color: 'white' } }}>Apply Now</Button>
 
-              </ListItem>
+            </ListItem>
           </List>
         </AccordionDetails>
       </Accordion>
-      
-      <Accordion    onChange={()=>chanegHandler('panel6')} expanded={accordion=='panel6'}>
+
+      <Accordion onChange={() => chanegHandler('panel6')} expanded={accordion == 'panel6'}>
         <AccordionSummary
-         expandIcon={ accordion=="panel6"?<RemoveIcon/>:<AddIcon/>} 
+          expandIcon={accordion == "panel6" ? <RemoveIcon /> : <AddIcon />}
           aria-controls="panel2a-content"
           id="panel2a-header"
-        
+
         >
           <Typography>Development</Typography>
         </AccordionSummary>
         <AccordionDetails>
-        <List>
-              <ListItem sx={{backgroundColor:'#FFFFFF',marginBottom:"5px",display:'flex',justifyContent:'space-between'}}>
-                  <Typography></Typography>
-                  <Button variant="outlined" sx={{borderRadius:'1px solid rgba(0, 0, 0, 0.1)',backgroundColor:'rgb(255, 255, 255)',border:'1px solid #80808080',color:'rgba(0, 0, 0, 0.6)',"&:hover":{backgroundColor:'#182F59',color:'white'}}}>Apply Now</Button>
+          <List>
+            <ListItem sx={{ backgroundColor: '#FFFFFF', marginBottom: "5px", display: 'flex', justifyContent: 'space-between' }}>
+              <Typography></Typography>
+              <Button variant="outlined" sx={{ borderRadius: '1px solid rgba(0, 0, 0, 0.1)', backgroundColor: 'rgb(255, 255, 255)', border: '1px solid #80808080', color: 'rgba(0, 0, 0, 0.6)', "&:hover": { backgroundColor: '#182F59', color: 'white' } }}>Apply Now</Button>
 
-              </ListItem>
-              <ListItem sx={{backgroundColor:'#FFFFFF',marginBottom:"5px",display:'flex',justifyContent:'space-between'}}>
-                  <Typography>Sales Executive</Typography>
-                  <Button variant="outlined" sx={{borderRadius:'1px solid rgba(0, 0, 0, 0.1)',backgroundColor:'rgb(255, 255, 255)',border:'1px solid #80808080',color:'rgba(0, 0, 0, 0.6)',"&:hover":{backgroundColor:'#182F59',color:'white'}}}>Apply Now</Button>
+            </ListItem>
+            <ListItem sx={{ backgroundColor: '#FFFFFF', marginBottom: "5px", display: 'flex', justifyContent: 'space-between' }}>
+              <Typography>Sales Executive</Typography>
+              <Button variant="outlined" sx={{ borderRadius: '1px solid rgba(0, 0, 0, 0.1)', backgroundColor: 'rgb(255, 255, 255)', border: '1px solid #80808080', color: 'rgba(0, 0, 0, 0.6)', "&:hover": { backgroundColor: '#182F59', color: 'white' } }}>Apply Now</Button>
 
-              </ListItem>
+            </ListItem>
           </List>
         </AccordionDetails>
       </Accordion>
-      </Container>
+    </Container>
 
-      
+
   </Box>
 }
